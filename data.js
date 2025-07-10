1567f27254e8eaf95f4d2a266011895a4394bb92
Ankit Kumar Tiwari <103519157+AnkitTiwari2005@users.noreply.github.com>
2025-07-10 16:05:30 +0530

    Update data.js

--- data.js
+++ data.js
@@ -229,19 +229,116 @@ const friends = [
   },
   {
     id: 5,
-    name: "Fifth Friend",
-    phone: "00000 00000",
+    name: "Khushi Tyagi",
+    phone: "99924 81747",
     timetable: {
       Monday: [
-        { start: "08:00", end: "09:00", type: "Class", title: "Sample Class", location: "Room-101", faculty: "Professor X" },
-        { start: "10:00", end: "11:00", type: "Break", title: "Break", location: "", faculty: "" }
+        { start: "09:30", end: "10:20", type: "Class", title: "Aptitude-III", location: "Block-B3-505", faculty: "Vinay Kumar" },
+        { start: "10:20", end: "11:10", type: "Class", title: "Aptitude-III", location: "Block-B3-505", faculty: "Vinay Kumar" },
+        { start: "11:20", end: "12:10", type: "Class", title: "Design and Analysis of Algorithms", location: "Block-B3-105", faculty: "Charnpreet Kaur" },
+        { start: "12:10", end: "13:00", type: "Break", title: "Break", location: "", faculty: "" },
+        { start: "13:05", end: "13:55", type: "Class", title: "Competitive Coding-I", location: "Block-B3-308-A", faculty: "Komal" },
+        { start: "13:55", end: "14:45", type: "Class", title: "Competitive Coding-I", location: "Block-B3-308-A", faculty: "Komal" },
+        { start: "14:45", end: "15:35", type: "Class", title: "Operating System", location: "Block-B3-105", faculty: "Puneet Kaur" },
+        { start: "15:35", end: "16:25", type: "Class", title: "Project Based Learning in Java", location: "Block-B3-105", faculty: "Radha" }
       ],
       Tuesday: [
-        { start: "09:00", end: "10:00", type: "Class", title: "Sample Class", location: "Room-102", faculty: "Professor Y" }
+        { start: "09:30", end: "10:20", type: "Class", title: "Design and Analysis of Algorithms", location: "Block-B3-105", faculty: "Charnpreet Kaur" },
+        { start: "10:20", end: "11:10", type: "Class", title: "Computer Networks", location: "Block-B3-105", faculty: "Kuldeep Kumar" },
+        { start: "11:20", end: "12:10", type: "Class", title: "Operating System", location: "Block-B3-105", faculty: "Puneet Kaur" },
+        { start: "12:10", end: "13:00", type: "Break", title: "Break", location: "", faculty: "" },
+        { start: "13:05", end: "13:55", type: "Class", title: "Project Based Learning in Java", location: "Block-B3-402-A", faculty: "Khushbu Cheetu" },
+        { start: "13:55", end: "14:45", type: "Class", title: "Project Based Learning in Java", location: "Block-B3-402-A", faculty: "Khushbu Cheetu" },
+        { start: "14:45", end: "15:35", type: "Class", title: "AI and CPS Lab for Automation", location: "Block-B3-208", faculty: "Parveen Badoni" },
+        { start: "15:35", end: "16:25", type: "Class", title: "AI and CPS Lab for Automation", location: "Block-B3-208", faculty: "Parveen Badoni" }
       ],
-      Wednesday: [],
-      Thursday: [],
-      Friday: []
+      Wednesday: [
+        { start: "09:30", end: "10:20", type: "Class", title: "Aptitude-III", location: "Block-B3-105", faculty: "Vinay Kumar" },
+        { start: "10:20", end: "11:10", type: "Class", title: "Aptitude-III", location: "Block-B3-105", faculty: "Vinay Kumar" },
+        { start: "11:20", end: "12:10", type: "Class", title: "Soft Skills-III", location: "Block-B3-107-A", faculty: "Nidhi Sood" },
+        { start: "12:10", end: "13:00", type: "Class", title: "Soft Skills-III", location: "Block-B3-107-A", faculty: "Nidhi Sood" },
+        { start: "13:05", end: "13:55", type: "Break", title: "Break", location: "", faculty: "" },
+        { start: "13:55", end: "14:45", type: "Class", title: "Computer Networks", location: "Block-B3-105", faculty: "Kuldeep Kumar" },
+        { start: "14:45", end: "15:35", type: "Class", title: "Competitive Coding-I", location: "Block-B3-105", faculty: "Manpreet Kaur" },
+        { start: "15:35", end: "16:25", type: "Class", title: "Competitive Coding-I", location: "Block-B3-105", faculty: "Manpreet Kaur" }
+      ],
+      Thursday: [
+        { start: "09:30", end: "10:20", type: "Class", title: "Computer Networks", location: "Block-B3-105", faculty: "Kuldeep Kumar" },
+        { start: "10:20", end: "11:10", type: "Class", title: "Design and Analysis of Algorithms", location: "Block-B3-105", faculty: "Charnpreet Kaur" },
+        { start: "11:20", end: "12:10", type: "Class", title: "Design and Analysis of Algorithms", location: "Block-B3-308", faculty: "Charnpreet Kaur" },
+        { start: "12:10", end: "13:00", type: "Class", title: "Design and Analysis of Algorithms", location: "Block-B3-308", faculty: "Charnpreet Kaur" },
+        { start: "13:05", end: "13:55", type: "Break", title: "Break", location: "", faculty: "" },
+        { start: "13:55", end: "14:45", type: "Class", title: "Operating System", location: "Block-B3-105", faculty: "Puneet Kaur" },
+        { start: "14:45", end: "15:35", type: "Class", title: "Project Based Learning in Java", location: "Block-B3-105", faculty: "Radha" },
+        { start: "15:35", end: "16:25", type: "Break", title: "Break", location: "", faculty: "" }
+      ],
+      Friday: [
+        { start: "09:30", end: "10:20", type: "Class", title: "Competitive Coding-I", location: "Block-B3-402", faculty: "Komal" },
+        { start: "10:20", end: "11:10", type: "Class", title: "Competitive Coding-I", location: "Block-B3-402", faculty: "Komal" },
+        { start: "11:20", end: "12:10", type: "Class", title: "AI and CPS Lab for Automation", location: "Block-B3-208", faculty: "Parveen Badoni" },
+        { start: "12:10", end: "13:00", type: "Class", title: "AI and CPS Lab for Automation", location: "Block-B3-208", faculty: "Parveen Badoni" },
+        { start: "13:05", end: "13:55", type: "Class", title: "Soft Skills-III", location: "Block-B3-107-B", faculty: "Nidhi Sood" },
+        { start: "13:55", end: "14:45", type: "Class", title: "Soft Skills-III", location: "Block-B3-107-B", faculty: "Nidhi Sood" },
+        { start: "14:45", end: "15:35", type: "Break", title: "Break", location: "", faculty: "" },
+        { start: "15:35", end: "16:25", type: "Break", title: "Break", location: "", faculty: "" }
+      ]
+    }
+  },
+  {
+    id: 6,
+    name: "Ujjwal Garg",
+    phone: "74578 10800",
+    timetable: {
+      Monday: [
+        { start: "09:30", end: "10:20", type: "Class", title: "Aptitude-III", location: "Block-B3-505", faculty: "Vinay Kumar" },
+        { start: "10:20", end: "11:10", type: "Class", title: "Aptitude-III", location: "Block-B3-505", faculty: "Vinay Kumar" },
+        { start: "11:20", end: "12:10", type: "Class", title: "Design and Analysis of Algorithms", location: "Block-B3-105", faculty: "Charnpreet Kaur" },
+        { start: "12:10", end: "13:00", type: "Break", title: "Break", location: "", faculty: "" },
+        { start: "13:05", end: "13:55", type: "Class", title: "Competitive Coding-I", location: "Block-B3-308-A", faculty: "Komal" },
+        { start: "13:55", end: "14:45", type: "Class", title: "Competitive Coding-I", location: "Block-B3-308-A", faculty: "Komal" },
+        { start: "14:45", end: "15:35", type: "Class", title: "Operating System", location: "Block-B3-105", faculty: "Puneet Kaur" },
+        { start: "15:35", end: "16:25", type: "Class", title: "Project Based Learning in Java", location: "Block-B3-105", faculty: "Radha" }
+      ],
+      Tuesday: [
+        { start: "09:30", end: "10:20", type: "Class", title: "Design and Analysis of Algorithms", location: "Block-B3-105", faculty: "Charnpreet Kaur" },
+        { start: "10:20", end: "11:10", type: "Class", title: "Computer Networks", location: "Block-B3-105", faculty: "Kuldeep Kumar" },
+        { start: "11:20", end: "12:10", type: "Class", title: "Operating System", location: "Block-B3-105", faculty: "Puneet Kaur" },
+        { start: "12:10", end: "13:00", type: "Break", title: "Break", location: "", faculty: "" },
+        { start: "13:05", end: "13:55", type: "Class", title: "Project Based Learning in Java", location: "Block-B3-402-A", faculty: "Khushbu Cheetu" },
+        { start: "13:55", end: "14:45", type: "Class", title: "Project Based Learning in Java", location: "Block-B3-402-A", faculty: "Khushbu Cheetu" },
+        { start: "14:45", end: "15:35", type: "Class", title: "AI and CPS Lab for Automation", location: "Block-B3-208", faculty: "Parveen Badoni" },
+        { start: "15:35", end: "16:25", type: "Class", title: "AI and CPS Lab for Automation", location: "Block-B3-208", faculty: "Parveen Badoni" }
+      ],
+      Wednesday: [
+        { start: "09:30", end: "10:20", type: "Class", title: "Aptitude-III", location: "Block-B3-105", faculty: "Vinay Kumar" },
+        { start: "10:20", end: "11:10", type: "Class", title: "Aptitude-III", location: "Block-B3-105", faculty: "Vinay Kumar" },
+        { start: "11:20", end: "12:10", type: "Class", title: "Soft Skills-III", location: "Block-B3-107-A", faculty: "Nidhi Sood" },
+        { start: "12:10", end: "13:00", type: "Class", title: "Soft Skills-III", location: "Block-B3-107-A", faculty: "Nidhi Sood" },
+        { start: "13:05", end: "13:55", type: "Break", title: "Break", location: "", faculty: "" },
+        { start: "13:55", end: "14:45", type: "Class", title: "Computer Networks", location: "Block-B3-105", faculty: "Kuldeep Kumar" },
+        { start: "14:45", end: "15:35", type: "Class", title: "Competitive Coding-I", location: "Block-B3-105", faculty: "Manpreet Kaur" },
+        { start: "15:35", end: "16:25", type: "Class", title: "Competitive Coding-I", location: "Block-B3-105", faculty: "Manpreet Kaur" }
+      ],
+      Thursday: [
+        { start: "09:30", end: "10:20", type: "Class", title: "Computer Networks", location: "Block-B3-105", faculty: "Kuldeep Kumar" },
+        { start: "10:20", end: "11:10", type: "Class", title: "Design and Analysis of Algorithms", location: "Block-B3-105", faculty: "Charnpreet Kaur" },
+        { start: "11:20", end: "12:10", type: "Class", title: "Design and Analysis of Algorithms", location: "Block-B3-308", faculty: "Charnpreet Kaur" },
+        { start: "12:10", end: "13:00", type: "Class", title: "Design and Analysis of Algorithms", location: "Block-B3-308", faculty: "Charnpreet Kaur" },
+        { start: "13:05", end: "13:55", type: "Break", title: "Break", location: "", faculty: "" },
+        { start: "13:55", end: "14:45", type: "Class", title: "Operating System", location: "Block-B3-105", faculty: "Puneet Kaur" },
+        { start: "14:45", end: "15:35", type: "Class", title: "Project Based Learning in Java", location: "Block-B3-105", faculty: "Radha" },
+        { start: "15:35", end: "16:25", type: "Break", title: "Break", location: "", faculty: "" }
+      ],
+      Friday: [
+        { start: "09:30", end: "10:20", type: "Class", title: "Competitive Coding-I", location: "Block-B3-402", faculty: "Komal" },
+        { start: "10:20", end: "11:10", type: "Class", title: "Competitive Coding-I", location: "Block-B3-402", faculty: "Komal" },
+        { start: "11:20", end: "12:10", type: "Class", title: "AI and CPS Lab for Automation", location: "Block-B3-208", faculty: "Parveen Badoni" },
+        { start: "12:10", end: "13:00", type: "Class", title: "AI and CPS Lab for Automation", location: "Block-B3-208", faculty: "Parveen Badoni" },
+        { start: "13:05", end: "13:55", type: "Class", title: "Soft Skills-III", location: "Block-B3-107-B", faculty: "Nidhi Sood" },
+        { start: "13:55", end: "14:45", type: "Class", title: "Soft Skills-III", location: "Block-B3-107-B", faculty: "Nidhi Sood" },
+        { start: "14:45", end: "15:35", type: "Break", title: "Break", location: "", faculty: "" },
+        { start: "15:35", end: "16:25", type: "Break", title: "Break", location: "", faculty: "" }
+      ]
     }
   }
 ];
